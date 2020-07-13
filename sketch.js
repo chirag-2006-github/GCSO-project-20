907ec89d1651de9f4011d98c91dec5beef2763f4
chirag-2006-github <66406410+chirag-2006-github@users.noreply.github.com>
2020-07-13 20:36:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var car,wall;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  car=createSprite(50,200,50,50);
+  
+
+speed=random(55,90);
+weight=random(400,1500);
+
+car.velocityX=speed;
+car.shapeColor="black";
+wall=createSprite(1200,200,60,height/2);
+  wall.shapeColor=color(80,80,80);
+
+}
+
+function draw() {
+  background("lightgreen");  
+
+  if(wall.X-car.X < (car.width+wall.width)/2){ 
+    car.velocityX=0;
+    var deformation=0.5*weight*speed*speed/22509;
+
+    if(deformation>180){
+      car.shapeColor="red";
+    
+    }
+
+    if(deformation<180 && deformation>100){
+      car.shapeColor="yellow";
+    }
+
+    if(deformation<100){
+      car.shapeColor="green";
+    }
+  }
+  drawSprites();
+}
